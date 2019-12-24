92e8aa478c6cb6619e1f8cabc9d953917a58aa4d
Stanley Umezuruike <manstylo@gmail.com>
2019-12-24 15:08:08 +0100

    Frame Animation

--- app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -3,14 +3,12 @@ package com.sriyank.animationsdemo
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 //import android.support.v7.app.AppCompatActivity
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var batteryAnimation: AnimationDrawable
-    private lateinit var signalAnimation: AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,27 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onStart() {
-        super.onStart()
-
-        targetImage.setBackgroundResource(R.drawable.battery_animation_list)
-        batteryAnimation = targetImage.background as AnimationDrawable
-        batteryAnimation.start()
-
-
-        imageWifi.setBackgroundResource(R.drawable.signal_animation_list)
-        signalAnimation = imageWifi.background as AnimationDrawable
-        signalAnimation.start()
-
-    }
-
     // Button click event handler 
-    fun startStopAnimation() {
-
-        if (batteryAnimation.isRunning)
-            batteryAnimation.stop()
-        else
-            batteryAnimation.start()
+    fun startStopAnimation(view: View) {
 
     }
 }
